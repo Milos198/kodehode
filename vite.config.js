19b00259d7af1c5bd52faebd71ce34108841a0b5
Milos198 <jmilos.j@gmail.com>
2026-02-02 13:19:29 +0100

    Fix movie.html bundling and paths

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/kodehode/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        movie: './public/movie.html'
+      }
+    }
+  }
 })
-
